1fcb4a7f9be73d491e94c95ab28d8ccc463ef116
Your Name <you@example.com>
2020-11-01 01:38:33 -0800

    sfsdf

--- network/network.sh
+++ network/network.sh
@@ -215,7 +215,7 @@ function explorerUp() {
     sed -i "s/ORG1_ADMIN_PRIVATE_KEY/${PRIV_KEY}/g" ./explorer/connection-profile/hyperledger-explorer.json
 
     # There are two bridges in the docker network, the 127.0.0.1 address remains an error when using the https protocol.
-    IPADDRESS=$(docker inspect orderer.example.com | awk 'NR == 278 {print $2}' | tr -d '",')
+    IPADDRESS=$(docker inspect orderer.example.com | awk 'NR == 268 {print $2}' | tr -d '",')
     sed -i "s/ORDERER_IP/${IPADDRESS}/g" ./explorer/connection-profile/hyperledger-explorer.json
     IPADDRESS=$(docker inspect peer0.org1.example.com | awk 'NR == 288 {print $2}' | tr -d '",')
     sed -i "s/PEER0ORG1_IP/${IPADDRESS}/g" ./explorer/connection-profile/hyperledger-explorer.json
